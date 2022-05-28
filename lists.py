9b1eb6b1193018a7031e816386af2e66458b71c7
vincentMunywoki <87576661+vincentMunywoki@users.noreply.github.com>
2022-05-28 12:56:34 +0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,1 +1,17 @@
+list1 = [1,2,3,4,5,6,7,8,9]
+
+#print 1--4, 6--8,
+#reverse the list
+#use pop
+#delete number 5
+#append 10,11,12
+#remove 6,7
+#print all the values of list1
+
+list2 = [50,5,10,0,20,6,30,40]
+
+#sort asccending
+#sort descending
+#find average
+#find sum
 
