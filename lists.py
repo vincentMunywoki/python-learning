00f87fea600b8489c20ff6c3a8fc299158f16843
vincentMunywoki <87576661+vincentMunywoki@users.noreply.github.com>
2022-05-28 14:39:35 +0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -15,3 +15,47 @@ list2 = [50,5,10,0,20,6,30,40]
 #find average
 #find sum
 
+list1 = [1,2,3,4,5,6,7,8,9]
+
+#print 1--4, 6--8,
+print(list1[0:4])
+print(list1[5:8])
+#reverse the list
+list1.reverse()
+print(list1)
+
+#use pop
+list1.pop(5)
+print(list1)
+
+#delete number 5
+del list1[4]
+list1.reverse()
+print(list1)
+
+#append 10,11,12
+list1.append([10,11,12])
+print(list1)
+
+#remove 6,7
+list1.remove(6)
+list1.remove(7)
+print(list1)
+
+#print all the values of list1
+print(list)
+list2 = [50,15,18.60,0,40,54,43]
+print(list2)
+#sort asccending 
+list2.sort()
+print(list2)
+#sort descending
+list2.reverse()
+print(list2)
+#find sum
+sum = sum(list2)
+print(sum)
+
+#find average
+def Average(list2):
+    print mean(list2))
