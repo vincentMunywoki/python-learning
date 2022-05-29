d86667ba7295ac4431669b3bf795bb623096686b
vincentMunywoki <87576661+vincentMunywoki@users.noreply.github.com>
2022-05-30 00:45:10 +0300

    Update If, else if and else statements.py

--- If, else if and else statements.py
+++ If, else if and else statements.py
@@ -42,3 +42,17 @@ elif len(name) > 50:
   print("name must be only of a maximun of 50 characters.")
 else:
   print("Name looks good!")
+
+#weight convertor project 
+
+weight = int(input('weight: '))
+unit = input('(L)bs or (k)g')
+if unit.upper() == ("L"):
+  converted = weight * 0.45
+  print(f"you are {converted} kilos")
+else:
+  converted = weight / 0.45
+  print(f"You are {converted} pounds")
+  
+
+
