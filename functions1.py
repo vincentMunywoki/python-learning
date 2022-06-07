efb88bd837bf854de1ecd779adc2ab2fbea4323c
vincentMunywoki <87576661+vincentMunywoki@users.noreply.github.com>
2022-06-07 12:33:01 +0300

    Create functions1.py

---
+++ functions1.py
@@ -0,0 +1,10 @@
+def my_school(): 
+  name = input("school name: ")
+  motto = input("school motto: ")
+  location = input("school location: ")
+  print("WELCOME to our school we are glad to have you")
+
+
+print("Yuo will never regret here")
+my_school()
+print("Knowledge transforms")
