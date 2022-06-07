3171b7fcf13f76990fd23307786379177c448807
vincentMunywoki <87576661+vincentMunywoki@users.noreply.github.com>
2022-06-07 13:13:38 +0300

    Create parameters.py

---
+++ parameters.py
@@ -0,0 +1,12 @@
+def greet_user(first_name, last_name): 
+  print(f"Hi {first_name} {last_name} !")
+  print("welcome abroad")
+  print("feel at home because")
+  print("we value you")
+
+
+print("start") 
+greet_user("john", "smith")
+greet_user("mary", "atieno")
+greet_user("Emilly", "mugendi")
+print("finish")
