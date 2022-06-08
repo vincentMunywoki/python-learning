01bcaed6a2b9f5dba755f9b1cf83b19a7697066c
vincentMunywoki <87576661+vincentMunywoki@users.noreply.github.com>
2022-06-08 09:26:58 +0300

    Create key_arguments.py

---
+++ key_arguments.py
@@ -0,0 +1,11 @@
+def greet_user(first_name, last_name): 
+  print(f"Hi {first_name} {last_name}!")
+  print("Welcome abroad")
+  print("We value your visit much.")
+
+  
+print("start")
+greet_user(first_name="Vincent", last_name="Kyengo")
+greet_user("Emmilly", last_name = "smith")
+greet_user(first_name= "Eliud",last_name = "morgan")
+print("finish")
