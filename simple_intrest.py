e341a888d1ba8d6078bd017774ceef9b44e1beb2
vincentMunywoki <87576661+vincentMunywoki@users.noreply.github.com>
2022-06-30 17:11:24 +0300

    Create simple_intrest.py

---
+++ simple_intrest.py
@@ -0,0 +1,13 @@
+def simple_intrest(p,t,r):
+  print("")
+  print("")
+  print("the simple principal is:", p)
+  print("The time period is:", t)
+  print("The rate of intrest is:", r)
+  si = (p*t*r)/100
+  print("The simple intrest is:", si)
+  return si
+simple_intrest(800,6,9)
+simple_intrest(700,9,8)
+simple_intrest(1000,5,12)
+simple_intrest(30000,6,20)
