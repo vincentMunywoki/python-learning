776a71a262af28b9f0cd516108d5229e12c235a9
vincentMunywoki <87576661+vincentMunywoki@users.noreply.github.com>
2022-07-05 15:06:24 +0300

    Create capitalize_method.py

---
+++ capitalize_method.py
@@ -0,0 +1,13 @@
+#capitalize method converts the first letter of sentence into uppercase and others into lower case. 
+#syntax ------> string.capitalize() 
+
+#example
+
+sentence = "i love PRograMMing."
+
+capitalize_string = sentence.capitalize()
+
+#string_sentence is not modified. 
+
+print('before captilize():',sentence) 
+print('after captalize():',capitalize_string)
