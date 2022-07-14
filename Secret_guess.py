c1573f8c4dd364fd26afe777a4f19e31451b0306
vincentMunywoki <87576661+vincentMunywoki@users.noreply.github.com>
2022-07-14 17:46:11 +0300

    Update Secret_guess.py

--- Secret_guess.py
+++ Secret_guess.py
@@ -4,6 +4,7 @@ secret_number = random.randint(0,10)
 guess_count = 0
 guess_limit = 3
 while guess_count < guess_limit: 
+   print("Quess any number between 0-10..")
    guess = int(input('guess: '))
    guess_count =  guess_count + 1 
    if guess == secret_number:
